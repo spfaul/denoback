7c13d179d0eccccc7cb78300fdc32e11f0d1565d
t0a5ted <flameburstunstoppable@gmail.com>
2023-06-08 18:18:34 +0800

    chore(client): format and serve

--- client/parallax.js
+++ client/parallax.js
@@ -1,44 +1,53 @@
 class ParallaxLayer {
-	constructor(layerData) {
-		this.layerData = layerData;
+  constructor(layerData) {
+    this.layerData = layerData;
     this.loaded = false;
-	}
+  }
 
-	load() {
+  load() {
     let loadedImgs = 0;
-    this.layerData.forEach((layer, i) => {
-      layer.img = loadImage(layer.imgName, img => {
+    this.layerData.layers.forEach((layer, i) => {
+      layer.img = loadImage(layer.imgName, (img) => {
         let s = new Sprite();
         s.collider = "none";
-        s.ani = loadAni(img, { frameSize: [img.width, img.height], frames: 1});
+        s.ani = loadAni(img, { frameSize: [img.width, img.height], frames: 1 });
         s.ani.stop();
         s.scale = { x: 3, y: 2.5 };
         s.x = 200;
         s.y = 500;
-        s.layer = i - this.layerData.length;
+        s.layer = i - this.layerData.layers.length;
         s.visible = false; // hide layers until all loaded
         layer.sprite = s;
         loadedImgs += 1;
-        if (loadedImgs === this.layerData.length) {
+        if (loadedImgs === this.layerData.layers.length) {
           this.loaded = true;
           // unhide all layers
-          for (let lay of this.layerData)
-            lay.sprite.visible = true;
+          for (let lay of this.layerData.layers) lay.sprite.visible = true;
         }
       });
     });
-	}
+  }
 
   update(playerPos) {
-    this.layerData.forEach((layer, i) => {
+    this.layerData.layers.forEach((layer, i) => {
       if (this.loaded) {
-        layer.sprite.y = lerp(layer.sprite.y, this.layerData.center.y + (playerPos.y - this.layerData.center.y) * layer.speed, this.layerData.smoothness);
-        layer.sprite.x = lerp(layer.sprite.x, this.layerData.center.x + (playerPos.x - this.layerData.center.x) * layer.speed, this.layerData.smoothness);
+        layer.sprite.y = lerp(
+          layer.sprite.y,
+          this.layerData.center.y +
+            (playerPos.y - this.layerData.center.y) * layer.speed,
+          this.layerData.smoothness
+        );
+        layer.sprite.x = lerp(
+          layer.sprite.x,
+          this.layerData.center.x +
+            (playerPos.x - this.layerData.center.x) * layer.speed,
+          this.layerData.smoothness
+        );
 
-        const layer_left_bound = layer.sprite.x - layer.sprite.w / 2
-        const layer_right_bound = layer.sprite.x + layer.sprite.w / 2
-        const layer_top_bound = layer.sprite.y - layer.sprite.h / 2
-        const layer_bottom_bound = layer.sprite.y + layer.sprite.h / 2
+        const layer_left_bound = layer.sprite.x - layer.sprite.w / 2;
+        const layer_right_bound = layer.sprite.x + layer.sprite.w / 2;
+        const layer_top_bound = layer.sprite.y - layer.sprite.h / 2;
+        const layer_bottom_bound = layer.sprite.y + layer.sprite.h / 2;
 
         if (layer_left_bound > camera.bound.min.x) {
           layer.sprite.x = camera.bound.min.x + layer.sprite.w / 2;
